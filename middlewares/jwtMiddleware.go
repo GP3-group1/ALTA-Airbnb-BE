54f817f0827cd8bbd9fca5a712a7a648acedf261
Kharisma Januar Muhammad JN <kharisma.januar@gmail.com>
2023-03-12 19:43:54 +0700

    change jwt middleware function

--- middlewares/jwtMiddleware.go
+++ middlewares/jwtMiddleware.go
@@ -2,65 +2,36 @@ package middlewares
 
 import (
 	"alta-airbnb-be/app/config"
-	"alta-airbnb-be/utils/consts"
-	"errors"
 	"time"
 
 	"github.com/golang-jwt/jwt/v4"
-	echoJwt "github.com/labstack/echo-jwt/v4"
+	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
 )
 
-//go:generate mockery --name JWTMiddleware_ --output ../mocks
-type JWTMiddleware_ interface {
-	ExtractToken(e echo.Context) (uint, string, error)
-}
-
-type JWT struct{}
-
-// ExtractToken implements JWTMiddleware_
-func (u *JWT) ExtractToken(e echo.Context) (uint, string, error) {
-	return ExtractToken(e)
-}
-
-func NewJWT() JWTMiddleware_ {
-	return &JWT{}
-}
-
 func JWTMiddleware() echo.MiddlewareFunc {
-	return echoJwt.WithConfig(echoJwt.Config{
+	return echojwt.WithConfig(echojwt.Config{
 		SigningKey:    []byte(config.SECRET_JWT),
 		SigningMethod: "HS256",
 	})
 }
 
-func CreateToken(userId int, userRole string) (string, error) {
+func CreateToken(userId uint) (string, error) {
 	claims := jwt.MapClaims{}
-	claims[consts.JWT_Authorized] = true
-	claims[consts.JWT_UserId] = userId
-	claims[consts.JWT_Role] = userRole
-	claims[consts.JWT_ExpiredTime] = time.Now().Add(time.Hour * 1).Unix()
-
+	claims["authorized"] = true
+	claims["userId"] = userId
+	claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString([]byte(config.SECRET_JWT))
+
 }
 
-func ExtractToken(e echo.Context) (uint, string, error) {
-	token, ok := e.Get("user").(*jwt.Token)
-	if !ok {
-		return 0, "", errors.New(consts.JWT_InvalidJwtToken)
+func ExtractTokenUserId(e echo.Context) uint {
+	user := e.Get("user").(*jwt.Token)
+	if user.Valid {
+		claims := user.Claims.(jwt.MapClaims)
+		userId := claims["userId"].(float64)
+		return uint(userId)
 	}
-	claims, ok := token.Claims.(jwt.MapClaims)
-	if !ok {
-		return 0, "", errors.New(consts.JWT_FailedCastingJwtToken)
-	}
-	if token.Valid {
-		loggedInUserId, existedUserId := claims[consts.JWT_UserId].(float64)
-		loggedInUserRole, existedUserRole := claims[consts.JWT_Role]
-		if !existedUserId || !existedUserRole {
-			return 0, "", errors.New(consts.SERVER_InternalServerError)
-		}
-		return uint(loggedInUserId), loggedInUserRole.(string), nil
-	}
-	return 0, "", errors.New(consts.SERVER_InternalServerError)
+	return 0
 }
