cb4488994d5a4abd5168919ddea834b0e7f55b2a
Kharisma Januar Muhammad JN <kharisma.januar@gmail.com>
2023-03-13 20:55:18 +0700

    create reservations entity

--- features/reservations/entity.go
+++ features/reservations/entity.go
@@ -1,1 +1,18 @@
 package reservations
+
+import (
+	"database/sql"
+	"time"
+)
+
+type ReservationEntity struct {
+	ID           uint
+	CheckInDate  sql.NullTime `validate:"required"`
+	CheckOutDate sql.NullTime `validate:"required"`
+	TotalNight   int
+	TotalPrice   int
+	RoomID       uint
+	UserID       uint
+	CreatedAt    time.Time
+	UpdatedAt    time.Time
+}
