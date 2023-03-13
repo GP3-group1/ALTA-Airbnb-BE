90dfad67f4d097968fe1a74881f6ea7f8ec98d18
Kharisma Januar Muhammad JN <kharisma.januar@gmail.com>
2023-03-13 23:44:47 +0700

    fix page and limit param in select all reservations handler

--- features/reservations/delivery/handler.go
+++ features/reservations/delivery/handler.go
@@ -6,6 +6,7 @@ import (
 	"alta-airbnb-be/utils/consts"
 	"alta-airbnb-be/utils/helpers"
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 )
@@ -49,9 +50,25 @@ func (reservationHandler *ReservationHandler) CheckReservation(c echo.Context) e
 // GetAllReservation implements reservations.ReservationDeliveryInterface_
 func (reservationHandler *ReservationHandler) GetAllReservation(c echo.Context) error {
 	userID := middlewares.ExtractTokenUserId(c)
-	page, limit, errParam := helpers.ExtractPageLimit(c)
-	if errParam != nil {
-		return c.JSON(http.StatusBadRequest, helpers.Response(errParam.Error()))
+	var page int = 1
+	pageParam := c.QueryParam("page")
+	if pageParam != "" {
+		pageConv, errConv := strconv.Atoi(pageParam)
+		if errConv != nil {
+			return c.JSON(http.StatusBadRequest, helpers.Response(consts.ECHO_InvaildPageParam))
+		} else {
+			page = pageConv
+		}
+	}
+	var limit int = 8
+	limitParam := c.QueryParam("limit")
+	if limitParam != "" {
+		limitConv, errConv := strconv.Atoi(limitParam)
+		if errConv != nil {
+			return c.JSON(http.StatusBadRequest, helpers.Response(consts.ECHO_InvaildLimitParam))
+		} else {
+			limit = limitConv
+		}
 	}
 
 	reservationEntity, errSelect := reservationHandler.reservationService.GetAll(page, limit, userID)
