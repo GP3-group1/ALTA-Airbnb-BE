f7865e94354889526c0bfde1bcba38ee2a957bda
Kharisma Januar Muhammad JN <kharisma.januar@gmail.com>
2023-03-13 21:11:04 +0700

    create entity to gorm, gorm to entity mapping for reservation

--- features/reservations/data/adapter.go
+++ features/reservations/data/adapter.go
@@ -1,1 +1,40 @@
 package data
+
+import (
+	"alta-airbnb-be/features/reservations"
+	_reservationModel "alta-airbnb-be/features/reservations/models"
+)
+
+func EntityToGorm(reservationEntity reservations.ReservationEntity) _reservationModel.Reservation {
+	reservationGorm := _reservationModel.Reservation{
+		CheckInDate:  reservationEntity.CheckInDate,
+		CheckOutDate: reservationEntity.CheckOutDate,
+		TotalNight:   reservationEntity.TotalNight,
+		TotalPrice:   reservationEntity.TotalPrice,
+		RoomID:       reservationEntity.RoomID,
+		UserID:       reservationEntity.UserID,
+	}
+	return reservationGorm
+}
+
+func GormToEntity(reservationGorm _reservationModel.Reservation) reservations.ReservationEntity {
+	return reservations.ReservationEntity{
+		ID:           reservationGorm.ID,
+		CheckInDate:  reservationGorm.CheckInDate,
+		CheckOutDate: reservationGorm.CheckOutDate,
+		TotalNight:   reservationGorm.TotalNight,
+		TotalPrice:   reservationGorm.TotalPrice,
+		RoomID:       reservationGorm.RoomID,
+		UserID:       reservationGorm.UserID,
+		CreatedAt:    reservationGorm.CreatedAt,
+		UpdatedAt:    reservationGorm.UpdatedAt,
+	}
+}
+
+func ListGormToEntity(reservationGorm []_reservationModel.Reservation) []reservations.ReservationEntity {
+	var reservationEntities []reservations.ReservationEntity
+	for _, v := range reservationGorm {
+		reservationEntities = append(reservationEntities, GormToEntity(v))
+	}
+	return reservationEntities
+}
--- features/reservations/entity.go
+++ features/reservations/entity.go
@@ -3,6 +3,8 @@ package reservations
 import (
 	"database/sql"
 	"time"
+
+	"github.com/labstack/echo/v4"
 )
 
 type ReservationEntity struct {
@@ -15,4 +17,26 @@ type ReservationEntity struct {
 	UserID       uint
 	CreatedAt    time.Time
 	UpdatedAt    time.Time
+}
+
+type ReservationInsert struct {
+	CheckInDate  sql.NullTime
+	CheckOutDate sql.NullTime
+}
+
+//go:generate mockery --name ReservationService_ --output ../../mocks
+type UserServiceInterface_ interface {
+	Create(input ReservationEntity) error
+}
+
+//go:generate mockery --name ReservationData_ --output ../../mocks
+type UserDataInterface_ interface {
+	Insert(input ReservationEntity) error
+}
+
+//go:generate mockery --name ReservationDelivery_ --output ../../mocks
+type UserDeliveryInterface_ interface {
+	AddReservation(c echo.Context) error
+	CheckReservation(c echo.Context) error
+	GetAllReservation(c echo.Context) error
 }
