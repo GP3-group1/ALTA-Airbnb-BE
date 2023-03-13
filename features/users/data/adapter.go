c733119fce6b4d539ac9f8743c48290534a22edb
Kharisma Januar Muhammad JN <kharisma.januar@gmail.com>
2023-03-13 14:06:47 +0700

    mapping entity to gorm users

--- features/users/data/adapter.go
+++ features/users/data/adapter.go
@@ -1,1 +1,34 @@
 package data
+
+import (
+	"alta-airbnb-be/features/users"
+	_userModel "alta-airbnb-be/features/users/models"
+)
+
+func EntityToGorm(userEntity users.UserEntity) _userModel.User {
+	userGorm := _userModel.User{
+		Name:        userEntity.Name,
+		Email:       userEntity.Email,
+		Password:    userEntity.Password,
+		Sex:         userEntity.Sex,
+		Address:     userEntity.Address,
+		PhoneNumber: userEntity.PhoneNumber,
+		Balance:     userEntity.Balance,
+	}
+	return userGorm
+}
+
+func GormToEntity(userGorm _userModel.User) users.UserEntity {
+	return users.UserEntity{
+		ID:          userGorm.ID,
+		Name:        userGorm.Name,
+		Email:       userGorm.Email,
+		Password:    userGorm.Password,
+		Sex:         userGorm.Sex,
+		Address:     userGorm.Address,
+		PhoneNumber: userGorm.PhoneNumber,
+		Balance:     userGorm.Balance,
+		CreatedAt:   userGorm.CreatedAt,
+		UpdatedAt:   userGorm.UpdatedAt,
+	}
+}
