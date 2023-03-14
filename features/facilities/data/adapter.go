f3ff000da07cd09b4474bda2685f3e11c3f82076
Andreas <Andreas.y45z@gmail.com>
2023-03-14 07:30:42 +0700

    Update facilities features

--- features/facilities/data/adapter.go
+++ features/facilities/data/adapter.go
@@ -1,1 +1,22 @@
 package data
+
+import (
+	"alta-airbnb-be/features/facilities"
+	_facilityModel "alta-airbnb-be/features/facilities/models"
+)
+
+func ConvertToGorm(facilityEntity *facilities.FacilityEntity) _facilityModel.Facility {
+	facilityModel := _facilityModel.Facility{
+		RoomID: facilityEntity.RoomID,
+		Name:   facilityEntity.Name,
+	}
+	return facilityModel
+}
+
+func ConvertToEntity(facilityModel *_facilityModel.Facility) facilities.FacilityEntity {
+	facilityEntity := facilities.FacilityEntity{
+		RoomID: facilityModel.RoomID,
+		Name:   facilityModel.Name,
+	}
+	return facilityEntity
+}
--- features/facilities/entity.go
+++ features/facilities/entity.go
@@ -1,1 +1,16 @@
-package reservations
+package facilities
+
+type FacilityEntity struct {
+	RoomID uint
+	Name   string
+}
+
+type FacilityRequest struct {
+	RoomID uint   `json:"room_id" form:"room_id"`
+	Name   string `json:"name" form:"name"`
+}
+
+type FacilityResponse struct {
+	RoomID uint   `json:"room_id"`
+	Name   string `json:"name"`
+}
