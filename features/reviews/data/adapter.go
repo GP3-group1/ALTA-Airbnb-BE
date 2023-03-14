e704f0d6d648e68be615ebee4a01eec06a96b086
Andreas <Andreas.y45z@gmail.com>
2023-03-14 07:34:09 +0700

    Update reviews features

--- features/reviews/data/adapter.go
+++ features/reviews/data/adapter.go
@@ -1,1 +1,39 @@
 package data
+
+import (
+	"alta-airbnb-be/features/reviews"
+	_reviewModel "alta-airbnb-be/features/reviews/models"
+)
+
+func ConvertToEntity(reviewModel _reviewModel.Review) *reviews.ReviewEntity {
+	reviewEntity := reviews.ReviewEntity{
+		UserID:  reviewModel.UserID,
+		RoomID:  reviewModel.RoomID,
+		Comment: reviewModel.Comment,
+		Rating:  reviewModel.Rating,
+	}
+	return &reviewEntity
+}
+
+func ConvertToEntities(reviewModels []_reviewModel.Review) []*reviews.ReviewEntity {
+	reviewEntities := []*reviews.ReviewEntity{}
+	for _, val := range reviewModels {
+		reviewEntities = append(reviewEntities, &reviews.ReviewEntity{
+			UserID:  val.UserID,
+			RoomID:  val.RoomID,
+			Comment: val.Comment,
+			Rating:  val.Rating,
+		})
+	}
+	return reviewEntities
+}
+
+func ConvertToGorm(reviewEntity *reviews.ReviewEntity) _reviewModel.Review {
+	reviewModel := _reviewModel.Review{
+		UserID:  reviewEntity.UserID,
+		RoomID:  reviewEntity.RoomID,
+		Comment: reviewEntity.Comment,
+		Rating:  reviewEntity.Rating,
+	}
+	return reviewModel
+}
--- features/reviews/entity.go
+++ features/reviews/entity.go
@@ -1,1 +1,22 @@
 package reviews
+
+type ReviewEntity struct {
+	UserID  uint
+	RoomID  uint
+	Comment string  `validate:"required"`
+	Rating  float64 `validate:"required"`
+}
+
+type ReviewRequest struct {
+	UserID  uint    `json:"user_id" form:"user_id"`
+	RoomID  uint    `json:"room_id" form:"room_id"`
+	Comment string  `json:"comment" form:"comment"`
+	Rating  float64 `json:"rating" form:"rating"`
+}
+
+type ReviewResponse struct {
+	UserID  uint    `json:"user_id"`
+	RoomID  uint    `json:"room_id"`
+	Comment string  `json:"comment"`
+	Rating  float64 `json:"rating"`
+}
