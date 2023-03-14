5d94424e443f39f0858425c5cbfbef85ff9fe4f3
Andreas <Andreas.y45z@gmail.com>
2023-03-14 07:21:52 +0700

    Update utils

--- utils/consts/room.go
+++ utils/consts/room.go
@@ -1,1 +1,26 @@
 package consts
+
+// Bind Error
+const (
+	ROOM_ErrorBindRoomData string = "error bind room data"
+)
+
+const (
+	// Validation
+	ROOM_InvalidInput string = "invalid room's input"
+
+	// Insert
+	ROOM_UserNotExisted  string = "user not existed"
+	ROOM_RoomNameAlreadyExisted string = "room's name already existed"
+)
+
+const (
+	// Register
+	ROOM_SuccessInsertRoomData string = "succesfully insert room data"
+
+	// Select
+	ROOM_SuccesReadRoomData string = "succesfully read room data"
+
+	// Delete
+	ROOM_SuccesDeleteRoomData string = "succesfully delete room data"
+)
