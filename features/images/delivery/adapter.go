3962b4119aac94f56dac23a86a45c50b545a3f7b
Andreas <Andreas.y45z@gmail.com>
2023-03-14 07:32:15 +0700

    Update images features

--- features/images/delivery/adapter.go
+++ features/images/delivery/adapter.go
@@ -1,1 +1,11 @@
 package delivery
+
+import "alta-airbnb-be/features/images"
+
+func RequestToEntity(imageRequest *images.ImageRequest) (images.ImageEntity) {
+	imageEntity := images.ImageEntity{
+		RoomID: imageRequest.RoomID,
+		Image:  imageRequest.Image,
+	}
+	return imageEntity
+}
--- features/images/entity.go
+++ features/images/entity.go
@@ -1,1 +1,19 @@
-package reservations
+package images
+
+import "mime/multipart"
+
+type ImageEntity struct {
+	RoomID uint
+	Url    string
+	Image  multipart.File
+}
+
+type ImageRequest struct {
+	RoomID uint           `json:"room_id" form:"room_id"`
+	Image  multipart.File `json:"image" form:"image"`
+}
+
+type ImageResponse struct {
+	RoomID uint           `json:"room_id"`
+	Image  multipart.File `json:"image"`
+}
