268bd562fcfed6c195381b99efa01bb61e33de07
Andreas <Andreas.y45z@gmail.com>
2023-03-15 18:17:26 +0700

    Add consts

---
+++ utils/consts/image.go
@@ -0,0 +1,21 @@
+package consts
+
+const (
+	IMAGE_ErrorBindImageData string = "error bind review data"
+)
+
+const (
+	// Validation
+	IMAGE_InvalidInput string = "invalid image's input"
+)
+
+const (
+	// Insert
+	IMAGE_SuccessInsertImageData string = "succesfully insert image data"
+
+	// Update
+	IMAGE_SuccessUpdateImageData string = "succesfully update image data"
+
+	// Delete
+	IMAGE_SuccessDeleteImageData string = "succesfully delete image data"
+)
