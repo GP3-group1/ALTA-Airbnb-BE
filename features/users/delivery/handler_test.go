05dc0d9e890049bc9c16903561e2cf29beb75fc0
Kharisma Januar Muhammad JN <kharisma.januar@gmail.com>
2023-03-16 20:42:55 +0700

    unit test: error get user data when select data

--- features/users/delivery/handler_test.go
+++ features/users/delivery/handler_test.go
@@ -5,6 +5,7 @@ import (
 	"alta-airbnb-be/middlewares"
 	"alta-airbnb-be/mocks"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
@@ -29,6 +30,10 @@ var (
 		UpdatedAt:   time.Now(),
 	}
 )
+
+type ResponseGlobal struct {
+	Message string
+}
 
 func TestGetUser(t *testing.T) {
 	e := echo.New()
@@ -62,6 +67,36 @@ func TestGetUser(t *testing.T) {
 			}
 			assert.Equal(t, http.StatusOK, rec.Code)
 			assert.Equal(t, returnData.Name, responseData.Name)
+		}
+		usecase.AssertExpectations(t)
+	})
+
+	t.Run("Failed when select data", func(t *testing.T) {
+		usecase.On("GetData", mock.Anything).Return(users.UserEntity{}, errors.New("error select data")).Once()
+		token, errToken := middlewares.CreateToken(returnData.ID)
+		if errToken != nil {
+			assert.Error(t, errToken)
+		}
+
+		srv := New(usecase)
+
+		req := httptest.NewRequest(http.MethodGet, "/users", nil)
+		req.Header.Set(echo.HeaderAuthorization, fmt.Sprintf("Bearer %v", token))
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		rec := httptest.NewRecorder()
+		echoContext := e.NewContext(req, rec)
+		echoContext.SetPath("/users")
+
+		var responseData ResponseGlobal
+
+		callFunc := middlewares.JWTMiddleware()(echo.HandlerFunc(srv.GetUserData))(echoContext)
+		if assert.NoError(t, callFunc) {
+			responseBody := rec.Body.String()
+			err := json.Unmarshal([]byte(responseBody), &responseData)
+			if err != nil {
+				assert.Error(t, err, "error")
+			}
+			assert.Equal(t, "error select data", responseData.Message)
 		}
 		usecase.AssertExpectations(t)
 	})
