6214b4892c3e377984e962cd65a969de29a3bd4a
Kharisma Januar Muhammad JN <kharisma.januar@gmail.com>
2023-03-16 20:57:05 +0700

    unit test: get user balance

--- features/users/delivery/handler_test.go
+++ features/users/delivery/handler_test.go
@@ -195,3 +195,70 @@ func TestInsert(t *testing.T) {
 		usecase.AssertExpectations(t)
 	})
 }
+
+func TestGetBalance(t *testing.T) {
+	e := echo.New()
+	usecase := new(mocks.UserService)
+	returnData := mock_data_user
+
+	t.Run("Success", func(t *testing.T) {
+		usecase.On("GetData", mock.Anything).Return(returnData, nil).Once()
+		token, errToken := middlewares.CreateToken(returnData.ID)
+		if errToken != nil {
+			assert.Error(t, errToken)
+		}
+
+		srv := New(usecase)
+
+		req := httptest.NewRequest(http.MethodGet, "/users/balances", nil)
+		req.Header.Set(echo.HeaderAuthorization, fmt.Sprintf("Bearer %v", token))
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		rec := httptest.NewRecorder()
+		echoContext := e.NewContext(req, rec)
+		echoContext.SetPath("/users/balances")
+
+		responseData := mock_data_user
+
+		callFunc := middlewares.JWTMiddleware()(echo.HandlerFunc(srv.GetUserBalance))(echoContext)
+		if assert.NoError(t, callFunc) {
+			responseBody := rec.Body.String()
+			err := json.Unmarshal([]byte(responseBody), &responseData)
+			if err != nil {
+				assert.Error(t, err, "error")
+			}
+			assert.Equal(t, http.StatusOK, rec.Code)
+			assert.Equal(t, returnData.Balance, responseData.Balance)
+		}
+		usecase.AssertExpectations(t)
+	})
+
+	t.Run("Failed when select data", func(t *testing.T) {
+		usecase.On("GetData", mock.Anything).Return(users.UserEntity{}, errors.New("error select data")).Once()
+		token, errToken := middlewares.CreateToken(returnData.ID)
+		if errToken != nil {
+			assert.Error(t, errToken)
+		}
+
+		srv := New(usecase)
+
+		req := httptest.NewRequest(http.MethodGet, "/users/balances", nil)
+		req.Header.Set(echo.HeaderAuthorization, fmt.Sprintf("Bearer %v", token))
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		rec := httptest.NewRecorder()
+		echoContext := e.NewContext(req, rec)
+		echoContext.SetPath("/users/balances")
+
+		var responseData ResponseGlobal
+
+		callFunc := middlewares.JWTMiddleware()(echo.HandlerFunc(srv.GetUserBalance))(echoContext)
+		if assert.NoError(t, callFunc) {
+			responseBody := rec.Body.String()
+			err := json.Unmarshal([]byte(responseBody), &responseData)
+			if err != nil {
+				assert.Error(t, err, "error")
+			}
+			assert.Equal(t, "error select data", responseData.Message)
+		}
+		usecase.AssertExpectations(t)
+	})
+}
